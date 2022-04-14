93fb98857eec0b5f92fd9026b3c209553cd51982
kahboom <rachelyordan@gmail.com>
2022-04-14 12:57:21 +0100

    color change

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
 const buttonStyling = {
-  backgroundColor: 'crimson',
+  // backgroundColor: 'crimson',
+  backgroundColor: 'BlueViolet',
   color: 'white',
   borderRadius: '25px',
   border: 0,
