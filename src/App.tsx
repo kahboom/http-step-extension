5c213b8f560a29832b421f4aa83ad4068da1e4a2
kahboom <rachelyordan@gmail.com>
2022-08-29 11:35:05 +0100

    move deps back to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import HttpStep from './HttpStep';
+import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import '@patternfly/patternfly/utilities/Display/display.css';
+import '@patternfly/patternfly/utilities/Flex/flex.css';
+import '@patternfly/patternfly/utilities/Sizing/sizing.css';
+import '@patternfly/patternfly/utilities/Spacing/spacing.css';
 
 // import '@patternfly/react-core/dist/styles/base.css';
 
--- src/HttpStep.tsx
+++ src/HttpStep.tsx
@@ -1,11 +1,5 @@
 import { HttpEndpoint } from './HttEndpoint';
 import SwaggerParser from '@apidevtools/swagger-parser';
-import '@patternfly/patternfly/patternfly.css';
-import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
-import '@patternfly/patternfly/utilities/Display/display.css';
-import '@patternfly/patternfly/utilities/Flex/flex.css';
-import '@patternfly/patternfly/utilities/Sizing/sizing.css';
-import '@patternfly/patternfly/utilities/Spacing/spacing.css';
 import {
   Form,
   FormGroup,
